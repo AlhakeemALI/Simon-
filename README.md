c44fc514e484cb54f8cdc81d1201a739d65b3093
alialhakeem <ali.b.alhakim@gmail.com>
2021-11-14 17:01:29 -0700

    added more js

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 SIMON GAME
+
 Description
+
 My app is called Simon. It's a web app version of the Digital Simon game. The program chooses a sequence of colors and makes the player repeat the same sequence to advance to the next round, with the sequence getting more difficult to follow in every round.The player's score from every round adds up as the game progresses. Users will have the option to play again.
 
 Technologies
+
 This app was created with HTML, CSS and Javascript.
--- app.js
+++ app.js
@@ -1,149 +1,149 @@
-
-let  playerChoices = [];
-let  computerChoices = [];
-let  gameStart = false;
+let playerChoices = [];
+let computerChoices = [];
+let gameStart = false;
 let win = false;
-let playerScore = 0;
-let round = 0;
-let winningScore = 20
+let round = 10;
+let canClick = false;
+let numLightToGuess = 0;
+let counter = 0;
 
+const startButton = document.querySelector(".but1");
+const divPanals = document.querySelectorAll(".panal");
+const resetButton = document.querySelector(".reset");
+const colorGreen = document.querySelector(".green");
+const colorRed = document.querySelector(".red");
+const colorYellow = document.querySelector(".yellow");
+const colorBlue = document.querySelector(".blue");
+const playerRound = document.querySelector("#round1");
+const score = document.querySelector("#score2");
 
-
-const startButton = document.querySelector('.but1')
-const  divPanals= document.querySelectorAll('.panal')
-const resetButton = document.querySelector('.reset')
-const  colorGreen = document.querySelector('.green');
-const colorRed = document.querySelector('.red');
-const colorYellow = document.querySelector('.yellow');
-const colorBlue = document.querySelector('.blue');
-const  playerRound = document.querySelector('#round1')
-
-
-startButton.addEventListener('click',(e) => {
-  gameStart = true
-  playerRound.textContent = 1
+startButton.addEventListener("click", (e) => {
+  gameStart = true;
+  canClick = true;
+  //computerTurn();
+  playerRound.textContent = 1;
   playerChoices = [];
   computerChoices = [];
-  flashAdd()
+  counter = 0;
+  numLightToGuess = 0;
+  flashAdd();
 
   //console.log(e.target)
-})
+});
 
-const randomColor = [
-  colorGreen, 
-  colorRed, 
-  colorYellow, 
-  colorBlue
-];
+const randomColor = [colorGreen, colorRed, colorYellow, colorBlue];
 
-const getRendamIndex  = (randomColor) => {
+const getRendamIndex = (randomColor) => {
+  const randomIndex =
+    randomColor[Math.floor(Math.random() * randomColor.length)];
+  return randomIndex;
+};
 
-  const randomIndex =  randomColor[Math.floor(Math.random() * randomColor.length)]
-  return randomIndex
-}
-
-
-const sequance = [
-  getRendamIndex(randomColor),
-  getRendamIndex(randomColor),
-  getRendamIndex(randomColor),
-  getRendamIndex(randomColor)
-  
-];
+const sequance = [getRendamIndex(randomColor)];
 
 //console.log(sequance)
 
-let canClick = false
-let numLightToGuess = 4
-let  counter = 0
 // add a Function to flash all the for panals
-const  addClass = circal => {
+const addClass = (circle) => {
   //console.log(circal.className)
-  if(circal.classList !== 'light' ) {
-    return new Promise((resolve,reject)  => {
-      computerChoices.push(parseInt(circal.id))
-      console.log(computerChoices)
-      circal.classList.add('light');
-      setTimeout(() => {
-        circal.classList.remove('light');
-        setTimeout(() => {
-          resolve();
-        },250);
-        counter++
-        if(counter >= numLightToGuess) {
-           canClick = true
-           console.log(playerChoices)
-            
+  if (circle.classList !== "light") {
+    return new Promise((resolve, reject) => {
+      circle.classList.add("light");
+      computerChoices.push(parseInt(circle.id));
+      console.log(`computerChoices  ===> ${[computerChoices]}`);
 
+      setTimeout(() => {
+        circle.classList.remove("light");
+        setTimeout(() => {
+          //computerChoices.shift(parseInt(circle.id));
+        }, 250);
+        resolve();
+        counter++;
+        console.log(`counter log ===> ${counter}`);
+        console.log(`num light to guess ===> ${numLightToGuess}`);
+        numLightToGuess += 1;
+        if (counter >= numLightToGuess && canClick == false) {
+          canClick = true;
+          //console.log(playerChoices);
         }
-      },1000);
+      }, 1000);
     });
   }
-  }
- 
-     const panalClick = divPanals.forEach((element) => {
-      element.addEventListener('click',function(event)  {
-          
-        if (canClick) {
-          playerChoices.push(parseInt(element.id))
-          console.log(playerChoices)
-          currentIndex = 0;
-         
-        }
-        
-      })
-       
-      })
-     
+};
 
-  const paly = () => {
-  //console.log(event)
-      if (!canClick) return 
-       if (event.target.classList == 'panal') {
-         const currentPanal = e.target.id;
-         
-         if (currentPanal === computerChoices[currentIndex]) {
-          playerChoices.push(parseInt())
-          currentIndex++
-          console.log(playerChoices)
-         }
-       } else {
-        currentIndex = 0
-       }
-        
-        
-      }
-      //paly()
-      //console.log(arrayOrder)
-       //sequance.push(getRendamIndex(randomColor))
-     //flashAdd();
-    
+const panalClick = divPanals.forEach((element) => {
+  element.addEventListener("click", function (event) {
+    //console.log(`click`);
+    if (canClick) {
+      playerChoices.push(parseInt(element.id));
+      //console.log(playerChoices);
 
-   
-
- const flashAdd = async () => {
-  for (const panal of sequance) {
-     await addClass(panal);
-  }
-  
-}
-
-     
-      
-  
-     const arraysEqual = ((array1, array2) => {
-      return Array.isArray(array1) && Array.isArray(array2) && array1.length === array2.length && array1.every((val, index) => val === array2[index])
-     })
-     //arraysEqual()
-
-     const checkWinner = () => {
-      arraysEqual(computerChoices,playerChoices )
-      currenIndex = 0;
-      playerChoices.length = 0;
-      computerChoices.length = 0;
-      round = 10
+      play();
     }
-    //checkWinner()
+  });
+});
+
+const play = () => {
+  if (!canClick) return;
+  //console.log("computerChoices  ===>" + [computerChoices]);
+  console.log(`playerChoices ${[playerChoices]}`);
+
+  if (arraysEqual(playerChoices, computerChoices)) {
+    //canClick = false;
+
+    score.textContent++;
+    playerRound.textContent++;
+    computerTurn();
+  } else if (!playerChoices.every((val) => computerChoices.includes(val))) {
+    canClick = false;
+    //computerChoices = [];
+    //playerChoices = [];
+
+    //counter = 0;
+    //numLightToGuess = 0;
+    console.log(`Else if it's working`);
+    resetButton.addEventListener("click", (e) => {
+      score.textContent = 0;
+      playerRound.textContent = 0;
+    });
+  }
+};
+
+const computerTurn = () => {
+  // computerChoices[i].push(getRendamIndex(randomColor))
+
+  sequance.push(getRendamIndex(randomColor));
+  computerChoices = [];
+  playerChoices = [];
+  counter = 0;
+  numLightToGuess = 0;
+  //playerRound.textContent++;
+  flashAdd();
+};
+
+const flashAdd = async () => {
+  for (const panal of sequance) {
+    await addClass(panal);
+  }
+};
+
+const arraysEqual = (array1, array2) => {
+  return (
+    Array.isArray(array1) &&
+    Array.isArray(array2) &&
+    array1.length === array2.length &&
+    array1.every((val, index) => val === array2[index])
+  );
+};
+//arraysEqual()
+
+const checkWinner = () => {
+  arraysEqual(computerChoices, playerChoices);
+  currenIndex = 0;
+  playerChoices.length = 0;
+  computerChoices.length = 0;
+  round = 10;
+};
+//checkWinner()
 
 //
-
