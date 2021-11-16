656452f1f3ab50f32c867c08233aa90d155f3f03
alialhakeem <ali.b.alhakim@gmail.com>
2021-11-15 18:41:00 -0700

    game is working

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 let playerChoices = [];
 let computerChoices = [];
+let sequance = [];
 let gameStart = false;
 let win = false;
-let round = 10;
 let canClick = false;
+let isGameRunning = false;
+let isResetOn = false;
 let numLightToGuess = 0;
 let counter = 0;
+let round = 10;
 
 const startButton = document.querySelector(".but1");
 const divPanals = document.querySelectorAll(".panal");
@@ -16,47 +19,39 @@ const colorYellow = document.querySelector(".yellow");
 const colorBlue = document.querySelector(".blue");
 const playerRound = document.querySelector("#round1");
 const score = document.querySelector("#score2");
+const resetText = document.querySelector("#resetText");
 
-startButton.addEventListener("click", (e) => {
+startButton.addEventListener("click", function (event) {
+  if (isGameRunning) {
+    return;
+  }
+  sequance = [];
+  isGameRunning = true;
   gameStart = true;
-  canClick = true;
-  //computerTurn();
+  isResetOn = true;
   playerRound.textContent = 1;
-  playerChoices = [];
-  computerChoices = [];
-  counter = 0;
-  numLightToGuess = 0;
-  flashAdd();
-
-  //console.log(e.target)
+  computerTurn();
 });
-
 const randomColor = [colorGreen, colorRed, colorYellow, colorBlue];
-
 const getRendamIndex = (randomColor) => {
   const randomIndex =
     randomColor[Math.floor(Math.random() * randomColor.length)];
   return randomIndex;
 };
 
-const sequance = [getRendamIndex(randomColor)];
-
-//console.log(sequance)
-
 // add a Function to flash all the for panals
 const addClass = (circle) => {
-  //console.log(circal.className)
   if (circle.classList !== "light") {
     return new Promise((resolve, reject) => {
       circle.classList.add("light");
-      computerChoices.push(parseInt(circle.id));
+      computerChoices.push(circle.id);
+      computerChoices.forEach((val) =>
+        console.log(`computerChoices===> ${typeof val}`)
+      );
       console.log(`computerChoices  ===> ${[computerChoices]}`);
-
       setTimeout(() => {
         circle.classList.remove("light");
-        setTimeout(() => {
-          //computerChoices.shift(parseInt(circle.id));
-        }, 250);
+        setTimeout(() => {}, 250);
         resolve();
         counter++;
         console.log(`counter log ===> ${counter}`);
@@ -64,7 +59,6 @@ const addClass = (circle) => {
         numLightToGuess += 1;
         if (counter >= numLightToGuess && canClick == false) {
           canClick = true;
-          //console.log(playerChoices);
         }
       }, 1000);
     });
@@ -73,11 +67,12 @@ const addClass = (circle) => {
 
 const panalClick = divPanals.forEach((element) => {
   element.addEventListener("click", function (event) {
-    //console.log(`click`);
     if (canClick) {
-      playerChoices.push(parseInt(element.id));
-      //console.log(playerChoices);
-
+      playerChoices.push(element.id);
+      playerChoices.forEach((val) =>
+        console.log(`playerChoices===> ${typeof val}`)
+      );
+      console.log(`playerChoices ====>  ${[playerChoices]}`);
       play();
     }
   });
@@ -85,39 +80,42 @@ const panalClick = divPanals.forEach((element) => {
 
 const play = () => {
   if (!canClick) return;
-  //console.log("computerChoices  ===>" + [computerChoices]);
-  console.log(`playerChoices ${[playerChoices]}`);
 
   if (arraysEqual(playerChoices, computerChoices)) {
-    //canClick = false;
-
     score.textContent++;
     playerRound.textContent++;
     computerTurn();
-  } else if (!playerChoices.every((val) => computerChoices.includes(val))) {
+  } else if (playerChoices.length == computerChoices.length) {
+    console.log(computerChoices);
+    console.log(playerChoices);
+    alert("Incorrect choice, start again...");
     canClick = false;
-    //computerChoices = [];
-    //playerChoices = [];
-
-    //counter = 0;
-    //numLightToGuess = 0;
-    console.log(`Else if it's working`);
-    resetButton.addEventListener("click", (e) => {
-      score.textContent = 0;
-      playerRound.textContent = 0;
-    });
+    console.log(`if ===> it's working`);
   }
 };
 
-const computerTurn = () => {
-  // computerChoices[i].push(getRendamIndex(randomColor))
+const reset = resetButton.addEventListener("click", function (e) {
+  console.log(`it's working`);
+  isResetOn = true;
+  computerChoices = [];
+  playerChoices = [];
+  sequance = [];
+  counter = 0;
+  numLightToGuess = 0;
+  score.textContent = 0;
+  playerRound.textContent = 0;
+  console.log(sequance);
+  isGameRunning = false;
+});
 
+const computerTurn = () => {
   sequance.push(getRendamIndex(randomColor));
   computerChoices = [];
   playerChoices = [];
+  //sequance = [];
   counter = 0;
   numLightToGuess = 0;
-  //playerRound.textContent++;
+
   flashAdd();
 };
 
@@ -135,15 +133,9 @@ const arraysEqual = (array1, array2) => {
     array1.every((val, index) => val === array2[index])
   );
 };
-//arraysEqual()
-
 const checkWinner = () => {
   arraysEqual(computerChoices, playerChoices);
-  currenIndex = 0;
   playerChoices.length = 0;
   computerChoices.length = 0;
   round = 10;
 };
-//checkWinner()
-
-//
